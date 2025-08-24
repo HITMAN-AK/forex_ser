55194a14a74ffababfee3a5194ee9d753585a402
Nithin-3 <legendaryairforce@gmail.com>
2025-08-24 19:04:42 +0530

    done

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from urllib.parse import quote_plus
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # password = quote_plus("Ashwin@01012004") 
 engine = create_engine(
     "mysql+mysqlconnector://B4eBKyw6HzmNeYc.root:01CceXjf0EhDag6d@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/forexprediction",
     connect_args={
-        "ssl_ca": r"C:\Forexprediction\forex_ser\ca.pem",
+        "ssl_ca": os.path.join(BASE_DIR,"ca.pem"),
         "ssl_verify_cert": True,
         "ssl_verify_identity": True
     }
